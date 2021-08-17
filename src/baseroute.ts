9398fda2dfa5fa73bda56cb2a2c1f642ad3f973b
Henry Moule <hen10@hotmail.co.uk>
2021-08-17 20:21:29 +0100

    build fix

--- src/baseroute.ts
+++ src/baseroute.ts
@@ -1,7 +1,6 @@
 let basename = '';
 
-// if (process.env.GITHUB_PAGES) {
-//   basename = `/${process.env.GITHUB_PAGES}`;
-// }
-console.log(basename, process.env.GITHUB_PAGES);
+if (process.env.GITHUB_PAGES) {
+  basename = `/${process.env.GITHUB_PAGES}`;
+}
 export default basename;
