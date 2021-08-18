35eb72b8debed28b0b4936213b01db95185bca4f
Henry Moule <hen10@hotmail.co.uk>
2021-08-18 21:34:28 +0100

    various changes

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,9 +1,8 @@
 import { FunctionalComponent, h } from 'preact';
 import AudioPlayer from 'material-ui-audio-player';
 import styles from './styles';
-import { AZUZD, ISEQSITN, MATSSENT, NTHUNA } from '../../constants';
+import { AZUZD, GODS_STORY, ISEQSITN, JESUS_FILM_URI, MATSSENT, NTHUNA } from '../../constants';
 import { Grid, Typography } from '@material-ui/core';
-import { useEffect, useRef } from 'preact/hooks';
 
 const Home: FunctionalComponent = () => {
     const classes = styles();
@@ -13,6 +12,8 @@ const Home: FunctionalComponent = () => {
     const qr4 = '/assets/images/19.jpg';
     const qr5 = '/assets/images/20.jpg';
     const qr6 = '/assets/images/21.jpg';
+    const rbbi = '/assets/images/14.jpg';
+    const amsiggel = '/assets/images/15.png';
     return (
         <div class={classes.root}>
           <Grid container spacing={2}>
@@ -35,9 +36,32 @@ const Home: FunctionalComponent = () => {
           </Grid>
           <Typography style={{marginTop: 40, marginBottom: 10}} variant="h5">awal n-rbbi</Typography>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={3}>
-            <Typography variant="h6">injil l-lmasih</Typography>
-              <div dangerouslySetInnerHTML={{__html: `<div class="arc-cont"><iframe src="https://api.arclight.org/videoPlayerUrl?refId=1_507-jf-0-0&apiSessionId=611b7188aaf785.85639629&player=bc.vanilla6&dtm=0&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><style>.arc-cont{position:relative;display:block;margin:10px auto;width:100%}.arc-cont:after{padding-top:59%;display:block;content:""}.arc-cont>iframe{position:absolute;top:0;bottom:0;right:0;left:0;width:98%;height:98%;border:0}</style></div>`}} />
+          <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" style={{marginBottom: 20}}>lkhbar ifulkin</Typography>
+
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" style={{marginBottom: 20}}>arratn l-lanbya</Typography>
+
+            </Grid>
+          </Grid>
+          <Typography style={{marginTop: 40, marginBottom: 10}} variant="h5">videos</Typography>
+          <Grid justifyContent="space-evenly" container spacing={2}>
+            <Grid style={{textAlign: 'center'}} item xs={12} sm={6} md={3}>
+              <Typography variant="h6">maylli iqsad rbbi</Typography>
+              <video style={{maxHeight: 200}} poster={rbbi} controls>
+                <source src={GODS_STORY} type="video/mp4" />
+              </video>
+            </Grid>
+            <Grid style={{textAlign: 'center'}} item xs={12} sm={6} md={3}>
+              <Typography variant="h6">amuddu n-umsiggel</Typography>
+             <a href="https://www.amsiggel.com/" target="_blank">
+               <img style={{maxHeight: 200}} src={amsiggel} />
+               </a>
+            </Grid>
+            <Grid style={{textAlign: 'center'}} item xs={12} sm={6} md={3}>
+              <Typography variant="h6">tudert l-lmasih</Typography>
+              <div dangerouslySetInnerHTML={{__html: '<div class="arc-cont"><iframe src="https://api.arclight.org/videoPlayerUrl?refId=1_507-jf-0-0&apiSessionId=611b7188aaf785.85639629&player=bc.vanilla6&dtm=0&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><style>.arc-cont{position:relative;display:block;margin:10px auto;width:100%}.arc-cont:after{padding-top:59%;display:block;content:""}.arc-cont>iframe{position:absolute;top:0;bottom:0;right:0;left:0;width:98%;height:98%;border:0}</style></div>'}} />
             </Grid>
           </Grid>
           <Grid container spacing={2} justifyContent="space-evenly">
