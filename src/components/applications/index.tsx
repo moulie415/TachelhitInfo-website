7716f7c90fdca3ae369c853f98326492d2de70c2
Henry Moule <hen10@hotmail.co.uk>
2021-09-12 23:01:43 +0100

    various changes

--- src/components/applications/index.tsx
+++ src/components/applications/index.tsx
@@ -23,12 +23,12 @@ const Applications: FunctionalComponent = () => (
     <Grid container justifyContent="space-evenly" style={{padding: 20}}>
       <Grid style={{ textAlign: 'center' }} item xs={12} sm={3} md={3}>
           <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr1} alt="qr code" />
-          <Typography variant="h6">awal i-wass</Typography>
+          <Typography variant="h6">awal i&#8209;wass</Typography>
           <Typography className={styles.appBody}>
-            ass f-wass
-            rad-ak-ntazn awal imimn
-            gh-warratn n-sidi rbbi.
-            sfeld-as ar-ttzaamt s-rrja ishan.
+            ass f&#8209;wass
+            rad&#8209;ak&#8209;ntazn awal imimn
+            gh&#8209;warratn n&#8209;sidi rbbi.
+            sfeld&#8209;as ar&#8209;ttzaamt s&#8209;rrja ishan.
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.wordofgodforeachday">
             <img src={playStore} width={QR_SIZE} alt="play store" />
@@ -39,10 +39,10 @@ const Applications: FunctionalComponent = () => (
           <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr2} alt="qr code" />
           <Typography variant="h6" className={styles.appTitleArabic} >اوال ءيواسّ</Typography>
           <Typography  className={styles.appBodyArabic}>
-            اسّ ف-واسّ
-            راد-اك-نتازن اوال ءيميمن
-            غ-وارّاتن ن-سيدي ربّي.
-            سفلد-اس ار-تّزعامت س-رّجا ءيصحان.
+            اسّ ف&#8209;واسّ
+            راد&#8209;اك&#8209;نتازن اوال ءيميمن
+            غ&#8209;وارّاتن ن&#8209;سيدي ربّي.
+            سفلد&#8209;اس ار&#8209;تّزعامت س&#8209;رّجا ءيصحان.
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.wordofgodforeachday.arabic">
             <img src={playStoreArabic} width={QR_SIZE} alt="play store arabic" />
@@ -53,11 +53,11 @@ const Applications: FunctionalComponent = () => (
       <Grid style={{ textAlign: 'center' }} item xs={12} sm={3} md={3}>
   
         <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr3} alt="qr code" />
-        <Typography variant="h6">amuddu n-umsiggel</Typography>
+        <Typography variant="h6">amuddu n&#8209;umsiggel</Typography>
         <Typography className={styles.appBody}>
-          lqist n-yan igummin 
-          lehna d-tayri d-sslamt,
-          iftu ar-sersn-isiggil.
+          lqist n&#8209;yan igummin 
+          lehna d&#8209;tayri d&#8209;sslamt,
+          iftu ar&#8209;sersn&#8209;isiggil.
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.amsiggel">
             <img src={playStore} width={QR_SIZE} alt="play store" />
@@ -69,9 +69,9 @@ const Applications: FunctionalComponent = () => (
         <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr4} alt="qr code" />
         <Typography variant="h6" className={styles.appTitleArabic} >امسيگّل</Typography>
           <Typography  className={styles.appBodyArabic}>
-            لقيست ن-يان ءيگومّين
-            لهنا ت-تايري د-سّلامت،
-            ءيفتو ار-سرسن-يسيگّيل.
+            لقيست ن&#8209;يان ءيگومّين
+            لهنا ت&#8209;تايري د&#8209;سّلامت،
+            ءيفتو ار&#8209;سرسن&#8209;يسيگّيل.
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.amsiggel.arabic">
             <img src={playStoreArabic} width={QR_SIZE} alt="play store arabic" />
@@ -81,10 +81,10 @@ const Applications: FunctionalComponent = () => (
           <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr5} alt="qr code" />
           <Typography variant="h6">tachelhit info</Typography>
           <Typography  className={styles.appBody}>
-            kchem s-tgmmi-negh tsunfut, 
-            ar-tsflidt i-lkhbar issfrahn, 
-            ar-taqrat iwaliwn mimnin, 
-            ar-tssmuqqult lfidyuwat fulkinin. 
+            kchem s&#8209;tgmmi&#8209;negh tsunfut, 
+            ar&#8209;tsflidt i&#8209;lkhbar issfrahn, 
+            ar&#8209;taqrat iwaliwn mimnin, 
+            ar&#8209;tssmuqqult lfidyuwat fulkinin. 
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.tachelhitinfo">
             <img src={playStore} width={QR_SIZE} alt="play store" />
@@ -94,10 +94,10 @@ const Applications: FunctionalComponent = () => (
           <img style={{ margin: 10 }} height={QR_SIZE} width={QR_SIZE} src={qr6} alt="qr code" />
           <Typography variant="h6" className={styles.appTitleArabic} >تاشلحيت ءينفو</Typography>
           <Typography  className={styles.appBodyArabic}>
-            كشم س-تگمّي-نغ تسونفوت،
-            ار-تسفليدت ءي-لخبار ءيسّفراحن،
-            ار-تاقرات ءيواليون ميمنين،
-            ار-تسّموقولت لفيديوات فولكينين.
+            كشم س&#8209;تگمّي&#8209;نغ تسونفوت،
+            ار&#8209;تسفليدت ءي&#8209;لخبار ءيسّفراحن،
+            ار&#8209;تاقرات ءيواليون ميمنين،
+            ار&#8209;تسّموقولت لفيديوات فولكينين.
           </Typography>
           <a href="https://play.google.com/store/apps/details?id=com.tachelhitinfo.arabic">
             <img src={playStoreArabic} width={QR_SIZE} alt="play store arabic" />
--- src/components/socialsFooter/index.tsx
+++ src/components/socialsFooter/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionalComponent, h } from 'preact';
-import {  Grid } from '@material-ui/core';
+import {  Button, Grid } from '@material-ui/core';
 import { Email, Facebook, WhatsApp } from '@material-ui/icons';
 import { PHONE_NUMBER } from '../../constants';
 
@@ -9,19 +9,25 @@ const SocialsFooter: FunctionalComponent = () => {
   return (
     <Grid  style={{marginBottom: 20}} container alignItems="center" justifyContent="center" spacing={3}>
       <Grid item>
-      <a href="https://www.facebook.com/tachelhit.info/" target="_blank" rel="noreferrer">
-        <Facebook style={{color: '#4267B2'}} fontSize="large" />
-        </a>
-        </Grid>
-        <Grid item>
-          <a href={`whatsapp://send?phone=${PHONE_NUMBER}`}> 
-        <WhatsApp style={{color: '#075e54'}}  fontSize="large" />
-        </a>
-        </Grid>
-        <Grid item>
-          <a style={{color: 'inherit'}} href="mailto:RD113267@swissmail.org">
-            <Email fontSize="large" />
+        <Button>
+          <a href="https://www.facebook.com/tachelhit.info/" target="_blank" rel="noreferrer">
+            <Facebook style={{color: '#4267B2', fontSize: 50}}/>
           </a>
+        </Button>
+        </Grid>
+        <Grid item>
+          <Button>
+            <a href={`whatsapp://send?phone=${PHONE_NUMBER}`}> 
+              <WhatsApp style={{color: '#075e54', fontSize: 50}} />
+            </a>
+          </Button>
+        </Grid>
+        <Grid item>
+           <Button>
+              <a style={{color: 'inherit'}} href="mailto:RD113267@swissmail.org">
+                <Email style={{fontSize: 50}} />
+              </a>
+          </Button>
         </Grid>
     </Grid>
 
