8ea1a8aa830dec6f58619be9a658f068395ada41
Henry Moule <hen10@hotmail.co.uk>
2021-12-01 21:27:12 +0000

    minor fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/ban-ts-ignore': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
--- src/components/socialsFooter/index.tsx
+++ src/components/socialsFooter/index.tsx
@@ -1,9 +1,12 @@
 import {FunctionalComponent, h} from 'preact';
-import {Button, Grid, Link} from '@material-ui/core';
+import {Button, Grid, Link, Typography} from '@material-ui/core';
 import {Email, Facebook, WhatsApp} from '@material-ui/icons';
 import {PHONE_NUMBER} from '../../constants';
+import {CustomHistory} from 'preact-router';
 
-const SocialsFooter: FunctionalComponent = () => {
+const SocialsFooter: FunctionalComponent<{history: CustomHistory}> = ({
+  history,
+}) => {
   return (
     <Grid
       style={{marginBottom: 20}}
@@ -12,29 +15,39 @@ const SocialsFooter: FunctionalComponent = () => {
       justifyContent="center">
       <Grid item style={{marginRight: 10, marginLeft: 10}}>
         <Button>
+          <Typography style={{textTransform: 'lowercase', marginRight: 5}}>
+            facebook
+          </Typography>
           <a
             href="https://www.facebook.com/tachelhit.info/"
             target="_blank"
             rel="noreferrer">
             <Facebook style={{color: '#4267B2', fontSize: 50}} />
           </a>
+          <Typography style={{marginLeft: 5, fontSize: 20}}>فيسبوك</Typography>
         </Button>
       </Grid>
       <Grid item style={{marginRight: 10, marginLeft: 10}}>
         <Button>
+          <Typography style={{textTransform: 'lowercase', marginRight: 5}}>
+            whatsapp
+          </Typography>
           <a href={`whatsapp://send?phone=${PHONE_NUMBER}`}>
             <WhatsApp style={{color: '#075e54', fontSize: 50}} />
           </a>
+          <Typography style={{marginLeft: 5, fontSize: 20}}>واتساب</Typography>
         </Button>
       </Grid>
       <Grid item style={{marginRight: 10, marginLeft: 10}}>
-        <Link href="email">
-          <Button>
-            <a style={{color: 'inherit'}} href="email">
-              <Email style={{fontSize: 50}} />
-            </a>
-          </Button>
-        </Link>
+        <Button onClick={() => history.push('Email')}>
+          <Typography style={{textTransform: 'lowercase', marginRight: 5}}>
+            email
+          </Typography>
+          <a style={{color: 'inherit'}} href="email">
+            <Email style={{fontSize: 50}} />
+          </a>
+          <Typography style={{marginLeft: 5, fontSize: 20}}>تابرات</Typography>
+        </Button>
       </Grid>
     </Grid>
   );
