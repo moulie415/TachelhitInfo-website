30a2e3623fba1c07c9a0024052c19bd5e3e775a8
Henry Moule <hen10@hotmail.co.uk>
2021-12-02 22:02:44 +0000

    documents changes

--- src/routes/documents/Documents.tsx
+++ src/routes/documents/Documents.tsx
@@ -1,9 +1,244 @@
+import {Button, Card, Grid, Typography} from '@material-ui/core';
 import {FunctionalComponent, h} from 'preact';
 import SocialsFooter from '../../components/socialsFooter';
 
 function Documents() {
   return (
-    <div>
+    <div
+      style={{
+        minHeight: '100%',
+        padding: 32,
+      }}>
+      <Grid container style={{marginBottom: 20}}>
+        <Grid item xs={12} sm={6} md={6}>
+          <Card
+            style={{
+              padding: 20,
+              marginRight: 10,
+              height: '100%',
+            }}>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5">
+                  التراث المسيحي في شمال إفريقيا
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              في كثير من أجزاء شمال إفريقيا توجد أطلال بنايات مسيحية عريقة. ترى
+              ماذا نعرف عن الحضارة المتقدِّمة والدِّين المتطور الذين تشهد لهما
+              هذه الآثار؟ وعلى رفوف خزاناتنا كتابات علماء مسيحيين من شمال
+              إفريقيا كأُغسطينوس وكبريانوس وترتوليانوس. ترى بماذا كان أسلافنا
+              هؤلاء يؤمنون؟
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  L'héritage chrétien en Afrique du Nord
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              Dans bien des régions de l’Afrique du Nord subsistent des ruines
+              d’anciens édifices chrétiens. Que savons-nous de la civilisation
+              complexe et la religion élaborée dont témoignent si clairement ces
+              restes ? Sur les rayons de nos bibliothèques nous rencontrons bon
+              nombre d’œuvres écrites par des savants nord-africains tels
+              Augustin, Cyprien et Tertullien.  Quelles étaient les croyances de
+              nos célèbres ancêtres ? Ce livre fascinant nous éclaire sur
+              une partie fondamentale de notre héritage culturel et religieux.
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  This Holy Seed
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              One of the great centres of evangelical Christianity in the age
+              after the apostles, NORTH AFRICA has become “the land of the
+              vanished church”. Four hundred years of dramatic Christian growth
+              gave way to slow decay and death. What were the secrets of
+              success? And what were the eventual causes of failure?
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <Card
+            style={{
+              padding: 20,
+              marginLeft: 10,
+              height: '100%',
+            }}>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  امودّو نومسيگّل
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              لقيست-اد، اي-ايتما، تگا تين يان ءيگومّين لهنا ت-تايري د-سّلامت،
+              ءيفتو ار-سرسن-يسيگّيل
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  amuddu n-umsiggel
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              lqist-ad, ay-aytma, tga tin yan igummin lhna t-tayri d-sslamt,
+              iftu ar-sersn-isiggil.
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography
+                  variant="h5"
+                  style={{textTransform: 'none', fontFamily: 'Tifinagh'}}>
+                  amuddu n-umsiggel
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{
+                paddingBottom: 20,
+                margin: '0 20%',
+                fontFamily: 'Tifinagh',
+                textAlign: 'center',
+              }}>
+              lqist-ad, ay-aytma, tga tin yan igummin lhna t-tayri d-sslamt,
+              iftu ar-sersn-isiggil.
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  Le voyage d’Amsiggel
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              L’histoire d’un jeune qui désire ardemment la paix, la bonté et la
+              sécurité, et qui se met en route à leur recherche.
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  The Seeker’s Quest
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              A story about a young man who longs for peace, kindness and
+              security, and sets out on a journey in search of them.
+            </Typography>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={6} md={6}>
+          <Card
+            style={{
+              padding: 20,
+              marginRight: 10,
+              height: '100%',
+            }}>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  اوال ءي-واسّ
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              اوال نربّي ءيكرايگاتّ اسّ
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  awal i-wass
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{paddingBottom: 20, margin: '0 20%', textAlign: 'center'}}>
+              awal nrbbi ikraygatt ass
+            </Typography>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography
+                  variant="h5"
+                  style={{textTransform: 'none', fontFamily: 'Tifinagh'}}>
+                  awal i-wass
+                </Typography>
+              </Button>
+            </div>
+            <Typography
+              style={{
+                paddingBottom: 20,
+                margin: '0 20%',
+                fontFamily: 'Tifinagh',
+                textAlign: 'center',
+              }}>
+              awal nrbbi ikraygatt ass
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <Card
+            style={{
+              padding: 20,
+              marginLeft: 10,
+              height: '100%',
+            }}>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  المرشد للإيمان المسيحي والحياة المسيحية
+                </Typography>
+              </Button>
+            </div>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  هل أنت مُستَعِدٌّ ؟
+                </Typography>
+              </Button>
+            </div>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  المسيحية في شمال إفريقيا: دروس من التاريخ
+                </Typography>
+              </Button>
+            </div>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  taølim el-kitâb el-mqaddas
+                </Typography>
+              </Button>
+            </div>
+            <div style={{width: '100%', textAlign: 'center'}}>
+              <Button>
+                <Typography variant="h5" style={{textTransform: 'none'}}>
+                  حياتي بحال زربية
+                </Typography>
+              </Button>
+            </div>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
